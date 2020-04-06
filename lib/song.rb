6ca27cf8b66930ea8391f6db7741f7104e80705b
Samiylo Kryshu <skryshu@gmail.com>
2020-04-06 20:06:40 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,33 @@
+class Song 
+  
+  attr_accessor :name, :artist 
+  
+  @@all = []
+  def initialize (name)
+    @name = name 
+    
+    @@all << self 
+  end
+  
+  def self.all 
+    @@all 
+  end
+  
+  def self.new_by_filename(file_name)
+    song = file_name.split(" - ")[1]
+    artist_name = file_name.split(" - ")[0]
+    new_song = self.new(song)
+    new_song.artist_name = artist_name
+    new_song
+    
+  end
+  
+  def artist_name=(name)
+    artist = Artist.find_or_create_by_name(name)
+    self.artist = artist
+    artist.add_song(self)
+    
+  end
+  
+  
+end
